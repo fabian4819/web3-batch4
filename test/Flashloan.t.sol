b42325f68048b2ac4ba2362e7598f6262c788b40
fabian4819 <habibfabianfahlesi2004@Mail.ugm.ac.id>
2025-05-10 15:21:41 +0700

    feat: flashloan test

---
+++ test/Flashloan.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Flashloan} from "../src/Flashloan.sol";
+
+contract FlashloanTest is Test {
+    Flashloan public flashloan;
+
+    address weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address usdc = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address aWeth = 0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8;
+
+    function setUp() public {
+        vm.createSelectFork("https://arb-mainnet.g.alchemy.com/v2/2hMC-guBxy1qlHFc8mi5i314PUIOSbrT", 335104487);
+        flashloan = new Flashloan();
+    }
+
+    function test_loopingSupply() public {
+        deal(weth, address(this), 1e18);
+
+        IERC20(weth).approve(address(flashloan), 1e18);
+
+        flashloan.loopingSupply(1e18, 2350e6);
+
+        assertGt(IERC20(aWeth).balanceOf(address(flashloan)), 1e18);
+        console.log("aWETH balance after looping supply: ", IERC20(aWeth).balanceOf(address(flashloan)));
+    }
+
+}
