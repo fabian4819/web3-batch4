bd4f9cae37bcb8a8a0113c444d0b1d8efaf82a06
fabian4819 <habibfabianfahlesi2004@Mail.ugm.ac.id>
2025-05-10 13:52:17 +0700

    feat: swap test

---
+++ test/Swap.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Swap} from "../src/Swap.sol";
+
+contract SwapTest is Test {
+    Swap public swap;
+
+    address weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address usdc = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+
+    function setUp() public {
+        vm.createSelectFork("https://arb-mainnet.g.alchemy.com/v2/2hMC-guBxy1qlHFc8mi5i314PUIOSbrT", 335104487);
+        swap = new Swap();
+    }
+
+    
+}
